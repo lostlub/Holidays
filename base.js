d92057b4c5207e9f6622394adbbcf26bf8a969b5
lostlub <ng9800923@gmail.com>
2026-04-13 16:43:03 +0530

    restart again

---
+++ base.js
@@ -0,0 +1,7 @@
+const red = document.getElementsByClassName("red")[0];
+const yellow = document.getElementsByClassName("yellow")[0];
+const green = document.getElementsByClassName("green")[0];
+
+red.addEventListener("click", function() {
+    
+}
